b8a483986a6634c62c85f1418d2e03a496d6d372
cki6090 <52525423+cki6090@users.noreply.github.com>
2022-11-18 15:47:48 +0900

    20221118

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  let name ="최광일";
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h2>안녕 리엑트 {name}</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
