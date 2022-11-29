cd25447e8816b1eabb3868da0fb7c4149097ad51
cki6090 <52525423+cki6090@users.noreply.github.com>
2022-11-29 14:28:28 +0900

    20221129

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,62 @@
-import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, NavLink } from 'react-router-dom';
+import { login, logout } from '../store/authSlice';
 
 const NavBar = () => {
-    return (
-        <nav className="navbar navbar-dark bg-dark">
-            <div className="container">
-                <Link className="navbar-brand" to="/">Home</Link>
-                <ul className="navbar-nav">
-                    <li className="nav-item">
-                        <Link className="nav-link active" aria-current="page" to="/blogs">
-                            Blogs
-                        </Link>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    )
+  const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
+  const dispatch = useDispatch();
+  return (
+    <nav className="navbar navbar-dark bg-dark">
+      <div className="container">
+        <Link className="navbar-brand" to="/">Home</Link>
+        <ul
+          style={{
+            flexDirection: 'row'
+          }}
+          className="navbar-nav"
+        >
+
+          <li className="nav-item me-2">
+            <button
+              className="text-white btn btn-link text-decoration-none"
+              onClick={() => {
+                if (isLoggedIn) {
+                  dispatch(logout());
+                } else {
+                  dispatch(login());
+                }
+              }}
+            >
+              {isLoggedIn ? 'Logout' : 'Login'}
+            </button>
+          </li>
+
+          {isLoggedIn ? <li className="nav-item me-2">
+            <NavLink
+              activeClassName="active"
+              className="nav-link"
+              aria-current="page"
+              to="/admin"
+            >
+              Admin
+            </NavLink>
+          </li> : null}
+
+          <li className="nav-it em">
+            <NavLink
+              activeClassName="active"
+              className="nav-link"
+              aria-current="page"
+              to="/blogs"
+            >
+              Blogs
+            </NavLink>
+          </li>
+
+        </ul>
+      </div>
+    </nav>
+  );
 };
-export default NavBar
+
+export default NavBar;
